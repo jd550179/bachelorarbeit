5ee0edde85afbf5a48be8adf7c56ae6afe38dc80
jordy <jordy.degens@rwth-aachen.de>
2016-10-20 16:18:02 +0200

    changes thomas asked to do

--- code/scripts/test.py
+++ code/scripts/test.py
@@ -3,10 +3,14 @@
 
 import ROOT
 
-from histfunction import hist
-g=ROOT.TFile("firsthistograms.root","recreate")
+from bachelorarbeit.code.histfunction import hist
+RF="firsthistograms.root"
+g=ROOT.TFile(RF,"recreate")
 g.Close()
-hist("/home/home2/institut_3b/degens/bachelorarbeit/CMSSW_7_4_7/src/bachelorarbeit/gaussians3.root","gaussians3","var2","hist1","firsthistograms.root",title="gaussdistribution",xtitle="x-axis",ytitle="y-axis")
-hist("/home/home2/institut_3b/degens/bachelorarbeit/CMSSW_7_4_7/src/bachelorarbeit/gaussians3.root","gaussians1","var3","hist2","firsthistograms.root",title="gaussdistribution",xtitle="x-axis",ytitle="y-axis")
+#red=ROOT.TColor.GetColor(204,7,30)
+hist("/home/home2/institut_3b/degens/bachelorarbeit/CMSSW_7_4_7/src/bachelorarbeit/gaussians3.root","gaussians3","var2","hist1",RF,title="gaussdistribution",xtitle="x-axis",ytitle="y-axis", linecolor=2,fillcolor=5)
+hist("/home/home2/institut_3b/degens/bachelorarbeit/CMSSW_7_4_7/src/bachelorarbeit/gaussians3.root","gaussians1","var3","hist2",RF,title="gaussdistribution",xtitle="x-axis",ytitle="y-axis")
+hist("/home/home2/institut_3b/degens/bachelorarbeit/CMSSW_7_4_7/src/bachelorarbeit/gaussians3.root","gaussians2","var0","hist3",RF,title="gaussdistribution",xtitle="x-axis",ytitle="y-axis",linecolor=2,fillcolor=20)
+
 
 
